d4f1d20c25dc6b2259d9c4311b10019a44a5dd51
avocado.codes <151567906+Niamh-Codes@users.noreply.github.com>
2024-02-23 21:16:45 +0000

    adding time blocking and organsing scheduling format

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -16,9 +16,39 @@ $(document).ready(function() {
         var currentDay = dayjs().format('dddd, MMMM D, YYYY');
         $('.dayTime').text(currentDay);
     }
+ //  generate time blocks for standard business hours
+ function generateTimeBlocks() {
+    var workdayStart = 9; // (9 AM)
+    var workdayEnd = 17; //  (5 PM)
+
+    var schedulerDiv = $('#scheduler');
+    var currentTime = dayjs().hour(); // Current hour
+
+    for (var i = workdayStart; i <= workdayEnd; i++) {
+        var timeBlock = $('<div class="row align-items-center"></div>');
+        var hourDiv = $('<div class="hour col-md-1"></div>').text(dayjs().hour(i).format('h A'));
+        var descriptionTextarea = $('<textarea class="description form-control col-md-10"></textarea>');
+
+        // Set different classes based on past, present, or future hour
+        if (i < currentTime) {
+            descriptionTextarea.addClass('past');
+        } else if (i === currentTime) {
+            descriptionTextarea.addClass('present');
+        } else {
+            descriptionTextarea.addClass('future');
+        }
+
+        var saveBtn = $('<button class="saveBtn btn btn-primary col-md-1"><i class="fa fa-save"></i></button>');
+
+        timeBlock.append(hourDiv, descriptionTextarea, saveBtn);
+        schedulerDiv.append(timeBlock);
+    }
+}
+
 
     // Call functions to initialize the page
     displayCurrentDay();
+    generateTimeBlocks();
 
     // Event listener for save buttons
     $('.saveBtn').on('click', saveEvent);
